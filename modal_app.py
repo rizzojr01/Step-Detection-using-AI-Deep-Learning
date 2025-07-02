a6282d9af0f50d07822f2f609194a4685e5f5b33
Surendhar <76396808+surendhar-palanisamy@users.noreply.github.com>
2025-07-02 20:16:26 +0530

    refactor: Remove min_containers parameter from app function decorator

--- modal_app.py
+++ modal_app.py
@@ -4,7 +4,7 @@ app = modal.App("step-detection-app")
 
 image = modal.Image.debian_slim().pip_install_from_requirements("requirements.txt")
 
-@app.function(image=image, min_containers=1)
+@app.function(image=image)
 @modal.asgi_app()
 def fastapi_app():
     from src.step_detection.api.api import app as fastapi_app
